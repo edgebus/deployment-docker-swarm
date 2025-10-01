ba4c94c451330d6593eca65897db6cce9c7c0271
Polina Tereschenko <polinatereschenko16@gmail.com>
2025-10-01 12:07:39 +0300

    Update script && template

--- misc/docker-entrypoint-default-certificate-generator.sh
+++ misc/docker-entrypoint-default-certificate-generator.sh
@@ -4,7 +4,7 @@
 apk add openssl
 
 if ! command -v openssl >/dev/null 2>&1; then
-    echo "OpenSSL is NOT installed. Cannot generate default certificate for organization '${ORGANIZATION}'. Pls, use proper Traefik image for the purpose." >&2
+    echo "OpenSSL is NOT installed. Cannot generate default certificate for organization '${DEFAULT_CERT_ORGANIZATION}'. Pls, use proper Traefik image for the purpose." >&2
     exit 1
 fi
 
@@ -12,19 +12,19 @@ openssl genrsa -out /root/ca.key 4096
 openssl genrsa -out /traefik-data/server.key 4096
 
 openssl req -new -x509 -days 3650 -subj \
- "/C=$COUNTRY/ST=$STATE/L=$STATE/O=$ORGANIZATION/OU=ololo/OU=kolya/CN=$COMMON_NAME/emailAddress=$EMAIL_ADDRESS" \
+ "/C=$DEFAULT_CERT_COUNTRY/ST=$DEFAULT_CERT_STATE/L=$DEFAULT_CERT_STATE/O=$DEFAULT_CERT_ORGANIZATION/OU=ololo/OU=kolya/CN=$DEFAULT_CERT_COMMON_NAME/emailAddress=$DEFAULT_CERT_EMAIL_ADDRESS" \
  -key /root/ca.key \
  -out /traefik-data/ca.crt
 openssl req -new -subj \
- "/C=$COUNTRY/ST=$STATE/L=$STATE/O=$ORGANIZATION/OU=$ORGANIZATION_UNIT/CN=$COMMON_NAME/emailAddress=$EMAIL_ADDRESS" \
+ "/C=$DEFAULT_CERT_COUNTRY/ST=$DEFAULT_CERT_STATE/L=$DEFAULT_CERT_STATE/O=$DEFAULT_CERT_ORGANIZATION/OU=$DEFAULT_CERT_ORGANIZATION_UNIT/CN=$DEFAULT_CERT_COMMON_NAME/emailAddress=$DEFAULT_CERT_EMAIL_ADDRESS" \
  -key /traefik-data/server.key \
  -out /root/server.csr
 
 cat <<EOF > /root/ca-ext.conf
 subjectAltName = @alt_names
 [alt_names]
-DNS.1 = $ROOT_DOMAIN
-DNS.2 = *.$ROOT_DOMAIN
+DNS.1 = $DEFAULT_CERT_ROOT_DOMAIN
+DNS.2 = *.$DEFAULT_CERT_ROOT_DOMAIN
 EOF
 
 openssl x509 -req -in /root/server.csr \
